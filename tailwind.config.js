77491bfe27324f21d0a8b2e5d9a93feaf6c3d6b5
mondaymood <hello@taraotto.com>
2023-01-16 14:00:23 +0700

    testing custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,22 @@ module.exports = {
     './*.{html,md,js}'
   ],
   theme: {
-    extend: {      
-    },
-    container: {
-      center: true,
-      padding: '1rem',
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'tahiti': {
+        100: '#cffafe',
+        200: '#a5f3fc',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#0e7490',
+        800: '#155e75',
+        900: '#164e63',
+      },
     },
 },
   plugins: [],
